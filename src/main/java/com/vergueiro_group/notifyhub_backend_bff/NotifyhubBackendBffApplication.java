93c9d6c01589294f91cbd50cd2778078edaa33d3
Vergueirod <diego.ssv@outlook.com>
2025-03-06 08:09:15 -0300

    Initial Commit

---
+++ src/main/java/com/vergueiro_group/notifyhub_backend_bff/NotifyhubBackendBffApplication.java
@@ -0,0 +1,13 @@
+package com.vergueiro_group.notifyhub_backend_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyhubBackendBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyhubBackendBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vergueiro_group/notifyhub_backend_bff/NotifyhubBackendBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vergueiro_group.notifyhub_backend_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyhubBackendBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
