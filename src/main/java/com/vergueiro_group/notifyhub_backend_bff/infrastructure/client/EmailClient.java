31762ac77a89b46ff580a338aeb4579b78564fdc
Vergueirod <diego.ssv@outlook.com>
2025-03-13 21:28:03 -0300

    Add cron

--- src/main/java/com/vergueiro_group/notifyhub_backend_bff/infrastructure/client/EmailClient.java
+++ src/main/java/com/vergueiro_group/notifyhub_backend_bff/infrastructure/client/EmailClient.java
@@ -2,10 +2,12 @@ package com.vergueiro_group.notifyhub_backend_bff.infrastructure.client;
 
 import com.vergueiro_group.notifyhub_backend_bff.business.dto.TaskDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notification", url = "${notification.url}")
 public interface EmailClient {
 
+    @PostMapping
     void sendEmail(@RequestBody TaskDTO dto);
 }
